d64ccec783da33982263d85dd1bfad2744896623
josanjohnata <josanjohnata@gmail.com>
2021-06-04 14:42:51 -0300

    Initial commit 'python basic course'

---
+++ meu_app.py
@@ -0,0 +1,34 @@
+# a = int(input('Entre com o primeiro valor: '))
+# b = int(input('Entre com o segundo valor: '))
+
+# soma = a + b
+# mut = a * b
+# div = a / b
+# sub = a - b
+#
+# print(soma, mut, div, sub)
+
+# resto_a = a % 2
+# resto_b = b % 2
+# if resto_a == 0 or resto_b == 0:
+#     print('foi digitado um número par')
+# else:
+#     print('nenhum número par foi digitado')
+
+a = int(input('Primeiro bimestre: '))
+if a > 10:
+    a = int(input('Você digitou errado.  Primeiro bimestre: '))
+b = int(input('Segundo bimestre: '))
+if b > 10:
+    b = int(input('Você digitou errado.  Segundo bimestre: '))
+c = int(input('Terceiro bimestre: '))
+if c > 10:
+    c = int(input('Você digitou errado.  Terceiro bimestre: '))
+d = int(input('Quarto bimestre: '))
+if d > 10:
+    d = int(input('Você digitou errado.  Quarto bimestre: '))
+media = (a + b + c + d) / 4
+if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+    print('media: {}'.format(media))
+# else:
+#     print('foi informado alguma nota errada')
