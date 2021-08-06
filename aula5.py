26f50a197c531832d8cd899562cb955fca23ef7e
Josan Johnata <64118585+josanjohnata@users.noreply.github.com>
2021-08-06 12:12:30 -0300

    Update aula5.py

--- aula5.py
+++ aula5.py
@@ -1,4 +1,4 @@
-lista = [1, 2, 23, -4, 5, 645, 9, 10];
+lista = [1, 2, 23, -4, 5, 645, 9, 10, 888];
 lista_animais = ['cachorro', 'gato', 'elefante', 'lobo', 'minha-sogra', 'Milk'];
 # print(lista, lista_animais[1])
 # soma = 0
