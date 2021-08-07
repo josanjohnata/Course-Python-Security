5c6acfe2163d5ea3420a4ecd1c1bfb6671ad636e
josanjohnata <josanjohnata@gmail.com>
2021-08-06 22:45:04 -0300

    Metodos

---
+++ aula7.py
@@ -0,0 +1,12 @@
+# Construções de métodos, funções e classes em python
+# Para usar os métodos precisamos definilos usando o "def"
+
+def soma(a, b):
+  return a + b
+
+def sub(a ,b):
+  return a - b
+
+  print(soma(3, 4))
+  print(soma(4, 5))
+  print(sub(2554,1582))
