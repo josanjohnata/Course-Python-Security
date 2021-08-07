7cc9456889d4e620bce9fa02169307f32aa1436b
josanjohnata <josanjohnata@gmail.com>
2021-08-06 22:48:02 -0300

    Metodos adiciona outros operandos

--- aula7.py
+++ aula7.py
@@ -5,8 +5,15 @@ def soma(a, b):
   return a + b
 
 def sub(a ,b):
-  return a - b
+  return a - 
+  
+def mult(a, b):
+  return a * b
 
-  print(soma(3, 4))
+def div(a, b):
+  return a / b
+
   print(soma(4, 5))
   print(sub(2554,1582))
+  print(mult(52, 25))
+  print(div(754, 52))
