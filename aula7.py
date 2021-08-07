f0adb1a3b8b55be4f0aa5cc8bd5fc78a8223d1de
josanjohnata <josanjohnata@gmail.com>
2021-08-06 22:58:19 -0300

    Metodos adiciona outros operandos

--- aula7.py
+++ aula7.py
@@ -1,19 +1,29 @@
 # Construções de métodos, funções e classes em python
 # Para usar os métodos precisamos definilos usando o "def"
 
-def soma(a, b):
-  return a + b
+#Class por conversão começa com letra maiúscula.
 
-def sub(a ,b):
-  return a - 
-  
-def mult(a, b):
-  return a * b
+class Calculadora:
+  def _init(self, num1, num2):
+    self.valorA = num1
+    self.valorB = num2
+  def soma(self):
+    return self.valorA + self.valorB
 
-def div(a, b):
-  return a / b
+  def sub(self):
+    return self.valorA - self.valorB
+    
+  def mult(self):
+    return self.valorA * self.valorB
 
-  print(soma(4, 5))
-  print(sub(2554,1582))
-  print(mult(52, 25))
-  print(div(754, 52))
+  def div(self):
+    return self.valorA / self.valorB
+
+calculadora = Calculadora(10, 2)
+print(calculadora.valorA)
+print(calculadora.valorB)
+
+  # print(soma(4, 5))
+  # print(sub(2554,1582))
+  # print(mult(52, 25))
+  # print(div(754, 52))
